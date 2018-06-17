3424cf7663639a48c8fcab280182abe81a40697a
Thomas Preusse <tpreusse@users.noreply.github.com>
2018-06-17 17:40:55 +0200

    hot reloading for dynamic components

--- test/pages/1.js
+++ test/pages/1.js
@@ -3,9 +3,23 @@ import React from 'react'
 import { renderMdast } from 'mdast-react-render'
 import { parse } from '@orbiting/remark-preset'
 import createSchema from '@project-r/styleguide/lib/templates/Article'
+import visit from 'unist-util-visit'
 
 import md from '../../../../Articles/article-talk-to-the-machine-1/article.md'
 
+import jsUrl from 'file-loader!../../build/part1.js'
+
 const schema = createSchema()
 
-export default () => renderMdast(parse(md), schema)
+const mdast = parse(md)
+
+visit(mdast, 'zone', node => {
+  if (node.identifier === 'DYNAMIC_COMPONENT') {
+    node.data.src = node.data.src +
+      '?u=' + encodeURIComponent(jsUrl)
+  }
+})
+
+export default () => <div key={jsUrl}>
+  {renderMdast(mdast, schema)}
+</div>
