c10bcaedc4a078ee1ca4cae23a145ffcb2e3086c
Thomas Preusse <tpreusse@users.noreply.github.com>
2018-06-20 20:36:15 +0200

    update translations

--- src/components/NeuralNetwork/index.js
+++ src/components/NeuralNetwork/index.js
@@ -3,7 +3,9 @@ import React from 'react'
 import { Interaction, RawHtml, Label } from '@project-r/styleguide'
 
 export default () => <div>
-  
+  <Interaction.H3>{t('nn/title')}</Interaction.H3>
+  <Interaction.P>{t('nn/description')}</Interaction.P>
+  <br />
 
   <RawHtml
     type={Label}
