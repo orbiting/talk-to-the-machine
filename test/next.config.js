99a4e4ca8c63c1e6c32ec2ee2afa8717f7667ac4
Thomas Preusse <tpreusse@users.noreply.github.com>
2018-06-26 01:51:03 +0900

    polyfills for local IE 11 testing

--- test/next.config.js
+++ test/next.config.js
@@ -6,6 +6,18 @@ module.exports = {
         use: 'raw-loader'
       }
     )
+
+    const originalEntry = config.entry
+    config.entry = async () => {
+      const entries = await originalEntry()
+
+      if (entries['main.js']) {
+        entries['main.js'].unshift('./lib/polyfill.js')
+      }
+
+      return entries
+    }
+
     return config
   }
 }
